3d12b903dbc79e43095cbf02dc25e1cf9b4fe7df
Linus Henze <LinusHenze@users.noreply.github.com>
2022-10-27 14:40:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "SwiftUtils", url: "https://github.com/pinauten/SwiftUtils", .branch("master")),
-        .package(name: "SwiftMachO", path: "../SwiftMachO")
+        .package(name: "SwiftMachO", url: "https://github.com/pinauten/SwiftMachO", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
